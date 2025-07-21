01c5eeac9fe4372fde487dadd75c7340ee3fcbba
Mahalakshmi <Mahalakshmi@LAPTOP-A5MQ66SC>
2025-07-21 07:56:36 -0700

    This is my if conditional program

---
+++ C2TC_CoreJavaPrograms/src/day1/assignment2/IfDemo.java
@@ -0,0 +1,13 @@
+package day1.assignment2;
+
+public class IfDemo {
+
+	public static void main(String[] args) {
+
+		int x = 3;
+		if (x != 5)
+			System.out.println("Value of x is not 5");
+		System.out.println("Value of x is 5");
+	}
+	
+}
