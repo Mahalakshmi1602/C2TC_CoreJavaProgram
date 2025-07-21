6379078179221703a011b95b736966359d23c3b5
Mahalakshmi <Mahalakshmi@LAPTOP-A5MQ66SC>
2025-07-21 07:58:27 -0700

    This is my ifelse program

---
+++ C2TC_CoreJavaPrograms/src/day1/assignment2/IfElseDemo.java
@@ -0,0 +1,19 @@
+package day1.assignment2;
+
+public class IfElseDemo {
+
+	public static void main(String[] args) {
+		int age= 21;
+		
+		System.out.println("if else demonstration");
+		
+		if(age>=18)
+			System.out.println(" You are eligible for Voting");	
+		else
+			System.out.println("Sorry! You need to wait more");
+
+		
+		
+	}
+	
+}
