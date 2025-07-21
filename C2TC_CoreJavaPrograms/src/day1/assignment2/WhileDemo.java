0e71cc9cd252eb09c52d49aa3ed47b1ea858eaee
Mahalakshmi <Mahalakshmi@LAPTOP-A5MQ66SC>
2025-07-21 08:08:40 -0700

    This is my While loop program.

---
+++ C2TC_CoreJavaPrograms/src/day1/assignment2/WhileDemo.java
@@ -0,0 +1,15 @@
+package day1.assignment2;
+
+public class WhileDemo {
+
+	public static void main(String[] args) 
+	{
+		int i= 5;
+		while(i<=10)
+		{
+			System.out.println(i);
+			i++;
+		}
+	}
+	
+}
