914828fe9d6bbce2a7df6c3b93bea6e5ead3e6a8
Mahalakshmi <Mahalakshmi@LAPTOP-A5MQ66SC>
2025-07-21 07:40:20 -0700

    This is my Do While Loop program.

---
+++ C2TC_CoreJavaPrograms/src/day1/assignment2/DoWhileLoop.java
@@ -0,0 +1,15 @@
+package day1.assignment2;
+
+public class DoWhileLoop {
+	public static void main(String[] args) {
+		
+		int i = 1;
+		do {
+			System.out.println(i);
+			i++;
+		} while (i <= 1);
+		
+		
+	}
+
+}
