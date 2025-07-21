08edeae80b5a060678b976fe4b0dd8353c0cec8c
Mahalakshmi <Mahalakshmi@LAPTOP-A5MQ66SC>
2025-07-21 07:38:06 -0700

    This is my if and else condition operator demo program

---
+++ C2TC_CoreJavaPrograms/src/day1/assignment2/Decisionmakingoperators.java
@@ -0,0 +1,23 @@
+package day1.assignment2;
+
+public class Decisionmakingoperators {
+	public static void main(String[] args) {
+		int x = 12, y = 6;
+		int a = 10;
+		int b = 8;
+
+		if (x >= y) {
+			System.out.println("true");
+		} else {
+			System.out.println("false");
+		}
+
+		if (!(a < b) || (a == b)) {
+			System.out.println("Condition is TRUE");
+		} else
+
+		{
+			System.out.println("Condition is FALSE");
+		}
+	}
+}
