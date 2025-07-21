a813dbc2961e46e8b7cd58b188f3b14314781812
Mahalakshmi <Mahalakshmi@LAPTOP-A5MQ66SC>
2025-07-21 08:45:41 -0700

    This is my program

---
+++ C2TC_CoreJavaPrograms/src/dayone/identifierDemo.java
@@ -0,0 +1,11 @@
+package dayone;
+
+public class identifierDemo {
+		public static void main(String[] args) {
+			int $number = 20;
+			System.out.println("Value of the number variable is: "+ $number);
+			
+			String studentName = "Maha";
+			System.out.println("Value is: "+ studentName);
+		}
+}
---
+++ C2TC_CoreJavaPrograms/src/daytwo/WhileDemo.java
@@ -0,0 +1,15 @@
+package daytwo;
+
+public class WhileDemo {
+
+	public static void main(String[] args) 
+	{
+		int i= 5;
+		while(i<=10)
+		{
+			System.out.println(i);
+			i++;
+		}
+	}
+	
+}
