bdef7f7a952a49d028fe5cc71cbfc285aec62235
Mahalakshmi <Mahalakshmi@LAPTOP-A5MQ66SC>
2025-07-21 03:36:29 -0700

    This my customer program with getter and setter method

--- C2TC_CoreJavaPrograms/src/day1/entity/CustomerDemo.java
+++ C2TC_CoreJavaPrograms/src/day1/entity/CustomerDemo.java
@@ -5,22 +5,37 @@ public class CustomerDemo {
 	public static void main(String[] args) {
 		//customer 1
 		Customer c1 = new Customer();
-		c1.cid = 101;
-		c1.cname = "raja";
-		c1.city = "Puducherry";
-		System.out.println("Customer id: "+c1.cid);
-		System.out.println("Customer name: "+c1.cname);
-		System.out.println("Customer city: "+c1.city);
+		c1.setCid(101);
+		c1.setCname("raja");
+		c1.setCity("Puducherry");
+		/*System.out.println("Customer id: "+c1.getCid());
+		System.out.println("Customer name: "+c1.getCname());
+		System.out.println("Customer city: "+c1.getCity());*/
+		
+		System.out.println(c1);
 		
 		//customer 2
 		Customer c2 = new Customer();
-		c2.cid = 102;
-		c2.cname = "priya";
-		c2.city = "chennai";
+		c2.setCid(102);
+		c2.setCname("priya");
+		c2.setCity("chennai");
 		System.out.println();
-		System.out.println("Customer id: "+c2.cid);
+		System.out.println(c2);
+		
+
+		//customer 3
+		Customer c3 = new Customer();
+		c3.setCid(103);
+		c3.setCname("Maha");
+		c3.setCity("Villupuram");
+		System.out.println();
+		System.out.println(c3);
+		
+		
+		/*System.out.println("Customer id: "+c2.cid);
 		System.out.println("Customer name: "+c2.cname);
 		System.out.println("Customer city: "+c2.city);
+		*/
 		
 		
 	}
