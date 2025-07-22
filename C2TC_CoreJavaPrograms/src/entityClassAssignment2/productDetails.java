e7f41432be7b9bedf6cc38d469a1472f7dd81871
Mahalakshmi <Mahalakshmi@LAPTOP-A5MQ66SC>
2025-07-22 00:46:54 -0700

    This is my product details program using entity class

---
+++ C2TC_CoreJavaPrograms/src/entityClassAssignment2/productDetails.java
@@ -0,0 +1,23 @@
+package entityClassAssignment2;
+
+import entityClassAssignment1.Students;
+
+public class productDetails {
+
+	public static void main(String[] args) {
+		Product s1 = new Product();
+		s1.setProductId(101);
+		s1.setProductName("raja");
+		s1.setPrice(21);
+		System.out.println(s1);
+		
+		Product s2 = new Product();
+		s2.setProductId(101);
+		s2.setProductName("raja");
+		s2.setPrice(21);
+		System.out.println(s2);
+		
+		
+
+	}
+}
