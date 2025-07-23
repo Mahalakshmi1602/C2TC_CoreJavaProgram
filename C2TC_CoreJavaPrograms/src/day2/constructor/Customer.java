ccac0a84fc25fe080f2cbc20eef0996f070ba31c
Mahalakshmi <Mahalakshmi@LAPTOP-A5MQ66SC>
2025-07-22 22:01:17 -0700

    This is my default and parameterized constructor

--- C2TC_CoreJavaPrograms/src/day2/constructor/Customer.java
+++ C2TC_CoreJavaPrograms/src/day2/constructor/Customer.java
@@ -7,17 +7,26 @@ public class Customer {
 		private String city;
 		public Customer()//default constructor
 		{
-			this.cid=101;
-			this.cname="Priya";
-			this.city="chennai";
+			System.out.println("Default constructor has been called ");
 		}
 		public Customer(int cid,String cname,String city)
 		//Parameterized constructor
 		{
+			this("unknown","puducherry");
 			this.cid=cid;
 			this.cname=cname;
 			this.city=city;
 		}
+		
+		public Customer(String cname,String city)
+		//Parameterized constructor
+		{
+		
+			this.cname=cname;
+			this.city=city;
+			System.out.println("Parameterized  constructor has been called ");
+		}
+		
 		public int getCid() {
 			return cid;
 		}
