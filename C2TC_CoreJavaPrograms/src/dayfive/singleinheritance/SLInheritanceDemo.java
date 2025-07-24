9c15a16f5b1cf597321f248d37671dcae0ee453b
Mahalakshmi <Mahalakshmi@LAPTOP-A5MQ66SC>
2025-07-24 06:07:59 -0700

    This is my Single inheritance program

---
+++ C2TC_CoreJavaPrograms/src/dayfive/singleinheritance/SLInheritanceDemo.java
@@ -0,0 +1,9 @@
+package dayfive.singleinheritance;
+
+public class SLInheritanceDemo {
+
+	public static void main(String[] args) {
+		Student student = new Student("Maha", "80203774", "Vpm", 976368231, 12, " MVIT college");
+		System.out.println(student);
+	}
+}
