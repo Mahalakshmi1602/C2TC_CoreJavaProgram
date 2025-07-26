a1c4f24d73da8820b25a26a4c86e092ff75fe552
Mahalakshmi <Mahalakshmi@LAPTOP-A5MQ66SC>
2025-07-25 19:24:16 -0700

    This is my banking system program

---
+++ C2TC_CoreJavaPrograms/src/Assignment3/Transactions.java
@@ -0,0 +1,24 @@
+package Assignment3;
+
+public final class Transactions {
+    private final double transactionFee = 10.0; 
+
+    public final void performTransaction(Account account, String type, double amount) {
+        System.out.println("\nPerforming " + type + " transaction...");
+        if (type.equalsIgnoreCase("deposit")) {
+            account.deposit(amount);
+            applyTransactionFee(account);
+        } else if (type.equalsIgnoreCase("withdraw")) {
+            account.withdraw(amount);
+            applyTransactionFee(account);
+        } else {
+            System.out.println("Invalid transaction type.");
+        }
+    }
+
+    private void applyTransactionFee(Account account) {
+        account.withdraw(transactionFee); 
+        System.out.println("Transaction fee of ₹" + transactionFee + " applied.");
+    }
+}
+
