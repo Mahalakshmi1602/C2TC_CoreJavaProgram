5239b2555eecb4f6b44dbd46c79b624bad8fd954
Mahalakshmi Manoharan <mahalakshmi.manohar2002@gmail.com>
2025-07-28 15:59:16 +0530

    This is Extending Interfae Program

---
+++ C2TC_CoreJavaPrograms/src/dayeight/interfaces/extendinginterfaces/ExtendingInterfaceDemo.java
@@ -0,0 +1,22 @@
+package dayeight.interfaces.extendinginterfaces;
+
+public class ExtendingInterfaceDemo implements ChildInterface {
+	
+	
+	
+	public void print() { 
+		System.out.println("print method");
+	}
+
+	
+	public void show() {
+		System.out.println("show Method");
+	}
+	
+	public static void main(String[] args) {
+        ExtendingInterfaceDemo obj = new ExtendingInterfaceDemo();
+        obj.print();  
+        obj.show();   
+    }
+	
+}
