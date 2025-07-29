dd62a7838fdaff5c7f5ad58cec1ce470c3c7a907
Mahalakshmi Manoharan <mahalakshmi.manohar2002@gmail.com>
2025-07-29 13:37:47 +0530

    This is my program

---
+++ C2TC_CoreJavaPrograms/src/dayeight/interfaces/markerinterfaces/MarketinterfaceDemo.java
@@ -0,0 +1,15 @@
+package dayeight.interfaces.markerinterfaces;
+
+public class MarketinterfaceDemo {
+
+	public static void main(String[] args) {
+		Student s=new Student(101,"Maha",16000,"Java");
+		if (s instanceof Registrable )
+			System.out.println("Student is registered ");
+		else
+			System.out.println("Student is not registered ");
+			
+
+	}
+
+}
